5fd000973015ce0658e00a56ad1c306ee02c0c8c
Jonathan Sandgren <jonathansandg@gmail.com>
2021-11-02 19:42:31 +0100

    OWN CODE STARTING HERE -- added api handler

---
+++ client/src/api.ts
@@ -0,0 +1,47 @@
+
+
+const API_URL = 'http://localhost:3000';
+
+interface PlayerData {
+  id: string;
+  name: string;
+}
+
+export const getPlayer = (id: string): Promise<PlayerData> => {
+  return fetch(`${API_URL}/player/${id}`)
+    .then(res => res.json());
+}
+
+export const getAllPlayers = (): Promise<PlayerData[]> => {
+  return fetch(`${API_URL}/players`)
+    .then(res => res.json());
+}
+
+export const addPlayer = (name: string): Promise<PlayerData> => {
+  return fetch(`${API_URL}/player`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name })
+  })
+  .then(res => res.json());
+}
+
+export const deletePlayer = (id: string): Promise<PlayerData> => {
+  return fetch(`${API_URL}/player/${id}`, {
+    method: 'DELETE'
+  })
+  .then(res => res.json());
+}
+
+export const updatePlayer = (id: string, name: string): Promise<PlayerData> => {
+  return fetch(`${API_URL}/player/${id}`, {
+    method: 'PUT', 
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name })
+  })
+  .then(res => res.json());
+}
