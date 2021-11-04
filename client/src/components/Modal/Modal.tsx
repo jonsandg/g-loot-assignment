296446ec3801192a9863bd5cefb7535b0796f4da
Jonathan Sandgren <jonathansandg@gmail.com>
2021-11-04 11:32:17 +0100

    Modal component

---
+++ client/src/components/Modal/Modal.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactModal, { Props } from 'react-modal';
+
+import styles from './Modal.module.scss';
+
+ReactModal.setAppElement('#root');
+
+interface ModalProps {
+  isOpen: boolean;
+  onRequestClose?(): any;
+  children: React.ReactNode;
+  actions?: React.ReactChild[];
+  title?: string;
+}
+
+export const Modal = ({ 
+  isOpen, 
+  onRequestClose, 
+  title,
+  actions,
+  children 
+}: ModalProps) => {
+  
+  return(
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName={styles.overlay}
+      className={styles.modal}
+    >
+      {title && (
+        <div className={styles.title}>
+          <h2>{title}</h2>
+        </div>
+      )}
+      <div className={styles.content}>
+        {children}
+      </div>
+      {actions && (
+        <div className={styles.actions}>
+          {actions}
+        </div>
+      )}
+    </ReactModal>
+  );
+}
--- client/src/pages/players/PlayersPage.tsx
+++ client/src/pages/players/PlayersPage.tsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import styles from './PlayersPage.module.scss';
 
+import styles from './PlayersPage.module.scss';
 import logo from 'images/gloot-logo.png';
 
 import { PlayerList } from 'components/PlayerList';
 import { Button } from 'components/Button';
+import { Modal } from 'components/Modal';
 
 import { getAllPlayers } from 'api';
-
 
 const PlayersPage = () => {
 
   const [players, setPlayers] = useState<PlayerData[]>([]);
   const [filterQuery, setFilterquery] = useState('');
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   useEffect(() => {
     getAllPlayers()
@@ -38,11 +39,26 @@ const PlayersPage = () => {
           value={filterQuery}
           onChange={e => setFilterquery(e.target.value)}
         />
-        <Button color="green" icon="add" />
+        <Button 
+          color="green" 
+          icon="add" 
+          onClick={() => setModalIsOpen(true)}
+        />
       </div>
       <div className={styles.listContainer}>
         <PlayerList players={filteredPlayers} />
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        title="Add New Player"
+        actions={[
+          <Button color="green">Add</Button>, 
+          <Button onClick={() => setModalIsOpen(false)}>Cancel</Button>
+        ]}
+      >
+        <p>Contenz</p>
+      </Modal>
     </div>
   );
 }
